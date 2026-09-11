8f6a882e0a5bb58bb665f2d546053f6f4292bf28
miduki-gif <midzuki1935@gmail.com>
2026-09-11 19:09:33 +0900

    topページへ戻るボタン追加

--- FramView_Front/src/pages/ResultPage.jsx
+++ FramView_Front/src/pages/ResultPage.jsx
@@ -58,7 +58,7 @@ export const ResultPage = () => {
                                         <Button variant="contained" sx={{ mt:5, ml:5, backgroundColor: '#ADF0C7', color: '#1F1F1F' }} onClick={nextPage}>次へ</Button>
                                     )}
                                     {currentIndex === categories.length -1 && (
-                                        <Button variant="contained" sx={{ mt:5, ml:5, backgroundColor: '#BFC5CA', color: '#1F1F1F' }} onClick={TopPageBack}>トップへ戻る</Button>
+                                        <Button variant="contained" sx={{ mt:5, ml:5, backgroundColor: '#ADF0C7', color: '#1F1F1F' }} onClick={TopPageBack}>トップへ戻る</Button>
                                     )}
                                 </>
                             )}
